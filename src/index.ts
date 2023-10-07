45a05d6e91813a860081d903acd8ca7fd3b152d5
TusharIbtekar <ibtekar8@gmail.com>
2023-10-07 22:51:18 +0600

    refactor: user registration refactor

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,14 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
-import { body, validationResult } from "express-validator";
-import AppDataSource from "./data-source";
-import { User } from "./entity/User";
 import express from "express";
+import { registerValidator } from "./validators/register.validator";
+import { UserController } from "./controllers/user.controller";
 
 require("reflect-metadata");
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.post(
-  "/register",
-  body("name").isString().isLength({ min: 2 }).not().isEmpty(),
-  body("password").isLength({ min: 3 }).not().isEmpty(),
-  body("email").isEmail().withMessage("Email must be valid"),
-  // body("lastname").isString().isLength({ min: 2 }),
-
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    let user = await User.findOne({ where: { email: req.body.email } });
-    if (user) {
-      return res.status(400).json({ errors: { msg: "User already exists" } });
-    } else {
-      const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-
-      user = User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword,
-      });
-      await AppDataSource.manager.save(user);
-      res.send(user);
-      // await AppDataSource.initialize();
-    }
-
-    // AppDataSource.destroy();
-  }
-);
+app.post("/register", registerValidator, UserController.register);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from App Engine!");
