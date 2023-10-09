9a6025d95e92fff66757b738f5c28caaf9a8b512
TusharIbtekar <ibtekar8@gmail.com>
2023-10-09 20:41:32 +0600

    refactor: auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,20 +5,52 @@ import { User } from "../entity/User";
 import { UserController } from "./user.controller";
 import { JWTController } from "./jwt.controller";
 
-export const AuthController = {
-  async register(req: Request, res: Response) {
-    let user = await UserController.getUserByEmail(req.body.email);
-    if (user) {
-      return res.status(400).json({ errors: { msg: "User already exists" } });
-    } else {
-      const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+export const register = async (req: Request, res: Response) => {
+  let user = await UserController.getUserByEmail(req.body.email);
+  if (user) {
+    return res.status(400).json({ errors: { msg: "User already exists" } });
+  } else {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
 
-      user = User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword,
-      });
+    user = User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
+    const token = JWTController.createToken({ email: user.email }, true);
+
+    res.cookie("refresh_token", token.refresh_token, {
+      expires: new Date(
+        Date.now() +
+          parseInt(process.env.JWT_REFRESH_TOKEN_EXPIRES_IN as string) *
+            30 *
+            24 *
+            360000
+      ),
+      httpOnly: true,
+    });
+
+    await AppDataSource.manager.save(user);
+    res.send({ user, access_token: token.access_token });
+  }
+
+  // AppDataSource.destroy();
+};
+
+export const login = async (req: Request, res: Response) => {
+  let user = await UserController.getUserByEmail(req.body.email);
+  if (!user) {
+    return res
+      .status(400)
+      .json({ errors: { msg: "You have to register first." } });
+  } else {
+    const validatePassword = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (validatePassword) {
       const token = JWTController.createToken({ email: user.email }, true);
 
       res.cookie("refresh_token", token.refresh_token, {
@@ -31,45 +63,11 @@ export const AuthController = {
         ),
         httpOnly: true,
       });
-
-      await AppDataSource.manager.save(user);
       res.send({ user, access_token: token.access_token });
-    }
-
-    // AppDataSource.destroy();
-  },
-
-  async login(req: Request, res: Response) {
-    let user = await UserController.getUserByEmail(req.body.email);
-    if (!user) {
-      return res
-        .status(400)
-        .json({ errors: { msg: "You have to register first." } });
     } else {
-      const validatePassword = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
-
-      if (validatePassword) {
-        const token = JWTController.createToken({ email: user.email }, true);
-
-        res.cookie("refresh_token", token.refresh_token, {
-          expires: new Date(
-            Date.now() +
-              parseInt(process.env.JWT_REFRESH_TOKEN_EXPIRES_IN as string) *
-                30 *
-                24 *
-                360000
-          ),
-          httpOnly: true,
-        });
-        res.send({ user, access_token: token.access_token });
-      } else {
-        return res.status(400).json({ errors: { msg: "Wrong password." } });
-      }
+      return res.status(400).json({ errors: { msg: "Wrong password." } });
     }
+  }
 
-    // AppDataSource.destroy();
-  },
+  // AppDataSource.destroy();
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { registerValidator } from "../validators/register.validator";
-import { AuthController } from "../controllers/auth.controller";
+import { login, register } from "../controllers/auth.controller";
 import { loginValidator } from "../validators/login.validator";
 
 const authRoutes = Router();
 
-authRoutes.post("/register", registerValidator, AuthController.register);
+authRoutes.post("/register", registerValidator, register);
 
-authRoutes.post("/login", loginValidator, AuthController.login);
+authRoutes.post("/login", loginValidator, login);
 
 export default authRoutes;
