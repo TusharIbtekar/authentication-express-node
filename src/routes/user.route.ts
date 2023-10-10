0062f5a37be25e56979be077900e96fc891d6b5b
TusharIbtekar <ibtekar8@gmail.com>
2023-10-11 01:41:18 +0600

    feat: add swagger api doc for users

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -8,6 +8,18 @@ import {
 import { updateUserValidator } from "../validators/updateUser.validator";
 
 const userRoutes = Router();
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     tags:
+ *      - Fetch all users
+ *     summary: Use to fetch all users
+ *     description: Use this endpoint to fetch every user.
+ *     responses:
+ *       '200':
+ *         description: Successfully fetched
+ */
 
 userRoutes.get("/", getUsers);
 
